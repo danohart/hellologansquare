dabf4e78d8dd8eb6237903f47a03c14eda15fa42
Daniel Hart <me@danielhart.co>
2023-04-01 15:54:18 -0500

    Updated privacy policy

--- pages/privacy-policy.js
+++ pages/privacy-policy.js
@@ -3,26 +3,26 @@ export default function PrivacyPolicy() {
     <div className='page'>
       <h1>Privacy Policy</h1>
       <p>
-        Your privacy is important to us. It is Hello Logan Square's policy to
-        respect your privacy regarding any information we may collect from you
-        across our website, http://helloavondale.co, and other sites we own and
-        operate.
+        Your privacy is important to us. It is Hello Logan Square&apos;s policy
+        to respect your privacy regarding any information we may collect from
+        you across our website, http://helloavondale.co, and other sites we own
+        and operate.
       </p>
       <p>
         We only ask for personal information when we truly need it to provide a
         service to you. We collect it by fair and lawful means, with your
-        knowledge and consent. We also let you know why we’re collecting it and
-        how it will be used.
+        knowledge and consent. We also let you know why we&apos;re collecting it
+        and how it will be used.
       </p>
       <p>
         We only retain collected information for as long as necessary to provide
-        you with your requested service. What data we store, we’ll protect
+        you with your requested service. What data we store, we&apos;ll protect
         within commercially acceptable means to prevent loss and theft, as well
         as unauthorized access, disclosure, copying, use or modification.
       </p>
       <p>
-        We don’t share any personally identifying information publicly or with
-        third-parties, except when required to by law.
+        We don&apos;t share any personally identifying information publicly or
+        with third-parties, except when required to by law.
       </p>
       <p>
         Our website may link to external sites that are not operated by us.
@@ -41,7 +41,7 @@ export default function PrivacyPolicy() {
         questions about how we handle user data and personal information, feel
         free to contact us.
       </p>
-      <p>This policy is effective as of 1 June 2020.</p>{" "}
+      <p>This policy is effective as of 1 April 2023.</p>
     </div>
   );
 }
